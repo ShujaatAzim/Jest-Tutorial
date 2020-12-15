adc3b08f8a0f3023baa226ef430a7698aa98daa1
Shujaat Azim <shujaat.azim@gmail.com>
2020-12-14 19:14:10 -0500

    I am doing more test
    I am done.

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,18 @@ import App from './App'
 import Counter from './Counter'
 import './setupTests'
 
+it("should render welcome message and the count component", () => {
+  const wrapper = shallow(<App />)
+  const welcome = wrapper.find('h1')
+  expect(welcome.exists()).toBe(true)
+})
+
+it("should render the counter component", () => {
+  const wrapper = shallow(<App />)
+  const counter = wrapper.find(<Counter />)
+  expect(counter.exists()).toBe(true)
+})
+
 it("contains the proper welcome message", () => {
   const wrapper = shallow(<App />)
   const welcome = <h1>Welcome to my test!</h1>
