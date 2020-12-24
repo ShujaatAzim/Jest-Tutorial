080ebaa1e7ccc73ae9ccd254589af680eb98fc80
Shujaat Azim <shujaat.azim@gmail.com>
2020-12-23 23:49:16 -0500

    Changed the count test to be more reuseable by snagging the current state first.

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -5,7 +5,8 @@ import './setupTests'
 
 it("increases the count by 2 after clicking Add twice", () => {
   const wrapper = shallow(<Counter />)
+  let current = wrapper.state().count
   wrapper.find("#add").simulate('click')
   wrapper.find("#add").simulate('click')
-  expect(wrapper.state().count).toEqual(2)
+  expect(wrapper.state().count).toEqual(current + 2)
 })
